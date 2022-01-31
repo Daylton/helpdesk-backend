776c54a8cb55b5d657c6e07b04945cc0928acff4
Daylton <dayltondb@gmail.com>
2022-01-31 16:43:43 -0300

    endpoint findAll para Tecnico

--- src/main/java/com/daylton/helpdesk/services/DBService.java
+++ src/main/java/com/daylton/helpdesk/services/DBService.java
@@ -30,13 +30,15 @@ public class DBService {
 	public void instanciaDB() {
 		Tecnico tec1 = new Tecnico(null, "Daylton", "69503275016", "daylton@email.com", "123");
 		tec1.addPerfil(Perfil.ADMIN);
+		
+		Tecnico tec2 = new Tecnico(null, "Pedro", "000000000", "pedro@email.com", "234");
 
 		Cliente cli1 = new Cliente(null, "Bruno Silva", "36836614025", "bruno@email.com", "123");
 
 		Chamado c1 = new Chamado(
 				null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
 		
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		tecnicoRepository.saveAll(Arrays.asList(tec1, tec2));
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		chamadoRepository.saveAll(Arrays.asList(c1));
 	}
