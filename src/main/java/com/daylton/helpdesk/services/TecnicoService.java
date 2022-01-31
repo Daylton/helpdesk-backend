3f108d7fd31f24331990a7b70f70ceb62786ea32
Daylton <dayltondb@gmail.com>
2022-01-31 19:39:40 -0300

    excecao para cpf e emails ja cadastrados

--- src/main/java/com/daylton/helpdesk/services/TecnicoService.java
+++ src/main/java/com/daylton/helpdesk/services/TecnicoService.java
@@ -6,17 +6,22 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.daylton.helpdesk.domain.Pessoa;
 import com.daylton.helpdesk.domain.Tecnico;
 import com.daylton.helpdesk.domain.dtos.TecnicoDTO;
+import com.daylton.helpdesk.repositories.PessoaRepository;
 import com.daylton.helpdesk.repositories.TecnicoRepository;
+import com.daylton.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.daylton.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
-	
+
 	@Autowired
 	private TecnicoRepository repository;
-	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
@@ -29,8 +34,22 @@ public class TecnicoService {
 
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
+	}
+
+	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema");
+		}
+
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema");
+		}
+
 	}
 
 }
